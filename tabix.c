9b14f2c1c222607da4651c3f27e085991b208062
Heng Li <lh3@live.co.uk>
2010-05-01 02:34:07 +0000

    * tabix-0.1.3-3 (r556) * fixed a small memory leak in knetfile * fixed a minor bug for remote downloading

--- tabix.c
+++ tabix.c
@@ -6,7 +6,7 @@
 #include "bgzf.h"
 #include "tabix.h"
 
-#define PACKAGE_VERSION "0.1.3-2 (r555)"
+#define PACKAGE_VERSION "0.1.3-3 (r556)"
 
 static int fetch_func(int l, const char *s, void *data)
 {
