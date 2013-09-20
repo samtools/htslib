70a227b146abe6014c2cf7c479fee2e31f2e0e7d
James Bonfield <jkb@sanger.ac.uk>
2013-09-20 10:00:28 +0100

    (io_lib r3460)
    Fixed curl timeout. We meant CONNECTTIMEOUT instead of TIMEOUT. This
    was causing long downloads to be terminated early.

--- htslib/cram/open_trace_file.c
+++ htslib/cram/open_trace_file.c
@@ -112,7 +112,7 @@ mFILE *find_file_url(char *file, char *url) {
 
     if (0 != curl_easy_setopt(handle, CURLOPT_URL, buf))
 	goto error;
-    if (0 != curl_easy_setopt(handle, CURLOPT_TIMEOUT, 10L))
+    if (0 != curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT, 60L))
 	goto error;
     if (0 != curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION,
 			      (curl_write_callback)mfwrite))
