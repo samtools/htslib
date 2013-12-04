d49f20eafc4eeaa945f41fb5a33677971e8d6713
Petr Danecek <pd3@sanger.ac.uk>
2013-12-04 16:47:01 +0000

    faidx: Detect compressed BGZF fasta files and create .gzi index without asking to allow proper functioning of samtools faidx fasta.fa.gz

--- faidx.c
+++ faidx.c
@@ -185,7 +185,9 @@ int fai_build(const char *fn)
 		free(str);
 		return -1;
 	}
+    if ( bgzf->is_compressed ) bgzf_index_build_init(bgzf);
 	fai = fai_build_core(bgzf);
+    if ( bgzf->is_compressed ) bgzf_index_dump(bgzf, fn, ".gzi");
 	bgzf_close(bgzf);
 	fp = fopen(str, "wb");
 	if ( !fp ) {
