3490604bafe23c6cf001a596f5784d65ebc95d85
Petr Danecek <pd3@sanger.ac.uk>
2014-03-27 13:12:07 +0000

    vcf: Propagate error in bcf_hdr_append

--- vcf.c
+++ vcf.c
@@ -517,6 +517,7 @@ int bcf_hdr_append(bcf_hdr_t *hdr, const char *line)
 {
     int len;
     bcf_hrec_t *hrec = bcf_hdr_parse_line(hdr, (char*) line, &len);
+    if ( !hrec ) return -1;
     if ( bcf_hdr_add_hrec(hdr, hrec) )
         bcf_hdr_sync(hdr);
     return 0;
