4d13ff04fa0aef0c7666ffa239ddfc973d3a0dcf
Petr Danecek <pd3@sanger.ac.uk>
2014-11-06 15:28:23 +0000

    Fix of memmove bug in bcf_remove_filter()

--- vcf.c
+++ vcf.c
@@ -2863,7 +2863,7 @@ int bcf_remove_filter(const bcf_hdr_t *hdr, bcf1_t *line, int flt_id, int pass)
         if ( flt_id==line->d.flt[i] ) break;
     if ( i==line->d.n_flt ) return 0;   // the filter is not present
     line->d.shared_dirty |= BCF1_DIRTY_FLT;
-    if ( i!=line->d.n_flt-1 ) memmove(line->d.flt+i,line->d.flt+i+1,line->d.n_flt-i);
+    if ( i!=line->d.n_flt-1 ) memmove(line->d.flt+i,line->d.flt+i+1,(line->d.n_flt-i-1)*sizeof(*line->d.flt));
     line->d.n_flt--;
     if ( !line->d.n_flt && pass ) bcf_add_filter(hdr,line,0);
     return 0;
