092b5aa471d28c300889fc782dc8bdd199b10394
John Marshall <jm18@sanger.ac.uk>
2017-01-03 11:34:15 +0000

    Happy New Year

--- htsfile.c
+++ htsfile.c
@@ -1,6 +1,6 @@
 /*  htsfile.c -- file identifier and minimal viewer.
 
-    Copyright (C) 2014-2016 Genome Research Ltd.
+    Copyright (C) 2014-2017 Genome Research Ltd.
 
     Author: John Marshall <jm18@sanger.ac.uk>
 
@@ -187,7 +187,7 @@ int main(int argc, char **argv)
         case 1:
             printf(
 "htsfile (htslib) %s\n"
-"Copyright (C) 2016 Genome Research Ltd.\n",
+"Copyright (C) 2017 Genome Research Ltd.\n",
                    hts_version());
             exit(EXIT_SUCCESS);
             break;
