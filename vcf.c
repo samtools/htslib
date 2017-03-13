8b3fbe036757d124f715593c790530d44f4ba379
James Blachly <james.blachly@gmail.com>
2017-03-13 20:55:51 +0000

    Silence implicit defn of alloca() warnings when not using GNU libc

--- vcf.c
+++ vcf.c
@@ -34,6 +34,23 @@ DEALINGS IN THE SOFTWARE.  */
 #include <stdint.h>
 #include <errno.h>
 
+#if HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
+#endif
+
 #include "htslib/vcf.h"
 #include "htslib/bgzf.h"
 #include "htslib/tbx.h"
