d8e5ec4b215ea4645472b3c93180b18bb7fc9e8e
Rob Davies <rmd+git@sanger.ac.uk>
2017-05-24 16:09:23 +0100

    Fix clang warning
    Stop clang from needlessly complaining:
    vcf.c:507:39: warning: adding 'int' to a string does not append to the string
    by swapping to code that's a bit more obvious as to how it works.

--- vcf.c
+++ vcf.c
@@ -503,14 +503,14 @@ int bcf_hdr_register_hrec(bcf_hdr_t *hdr, bcf_hrec_t *hrec)
     if (hrec->type == BCF_HL_INFO || hrec->type == BCF_HL_FMT) {
         if (type == -1) {
             if (hts_verbose >= 2)
-                fprintf(stderr, "[E::%s] A%s %s field has no Type defined. Assuming String\n",
-                        __func__, "n" + (*hrec->key != 'I'), hrec->key);
+                fprintf(stderr, "[E::%s] %s %s field has no Type defined. Assuming String\n",
+                        __func__, *hrec->key == 'I' ? "An" : "A", hrec->key);
             type = BCF_HT_STR;
         }
         if (var == -1) {
             if (hts_verbose >= 2)
-                fprintf(stderr, "[E::%s] A%s %s field has no Number defined. Assuming '.'\n",
-                        __func__, "n" + (*hrec->key != 'I'), hrec->key);
+                fprintf(stderr, "[E::%s] %s %s field has no Number defined. Assuming '.'\n",
+                        __func__, *hrec->key == 'I' ? "An" : "A", hrec->key);
             var = BCF_VL_VAR;
         }
     }
