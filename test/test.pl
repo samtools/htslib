samtools/htslib

commit a13ac99eae872440a21153db7b7c8a8f8f6c1b8e

Optimisation for mutithreaded uncompressed bgzf
Eliminates a memcpy from the uncompressed to compressed buffer.

bgzf.c
@@ -1034,6 +1034,39 @@ void *bgzf_encode_func(void *arg) {
     return arg;
 }
 
+// Optimisation for compression level 0 (uncompressed deflate blocks)
+// Avoids memcpy of the data from uncompressed to compressed buffer.
+void *bgzf_encode_level0_func(void *arg) {
+    bgzf_job *j = (bgzf_job *)arg;
+    uint32_t crc;
+    j->comp_len = j->uncomp_len + BLOCK_HEADER_LENGTH + BLOCK_FOOTER_LENGTH + 5;
+
+    // Data will have already been copied in to
+    // j->comp_data + BLOCK_HEADER_LENGTH + 5
+
+    // Add preamble
+    memcpy(j->comp_data, g_magic, BLOCK_HEADER_LENGTH);
+    u16_to_le(j->comp_len, j->comp_data + 16);
+
+    // Deflate uncompressed data header
+    j->comp_data[BLOCK_HEADER_LENGTH] = 1; // BFINAL=1, BTYPE=00; see RFC1951
+    u16_to_le(j->uncomp_len, j->comp_data + BLOCK_HEADER_LENGTH + 1);
+    u16_to_le(~j->uncomp_len, j->comp_data + BLOCK_HEADER_LENGTH + 3);
+
+    // Trailer (CRC, uncompressed length)
+#ifdef HAVE_LIBDEFLATE
+    crc = libdeflate_crc32(0, j->comp_data + BLOCK_HEADER_LENGTH + 5,
+                           j->uncomp_len);
+#else
+    crc = crc32(crc32(0L, NULL, 0L),
+                (Bytef*)j->comp_data + BLOCK_HEADER_LENGTH + 5, j->uncomp_len);
+#endif
+    u32_to_le(crc, j->comp_data +  j->comp_len - 8);
+    u32_to_le(j->uncomp_len, j->comp_data + j->comp_len - 4);
+
+    return arg;
+}
+
 // Our input block has already been decoded by bgzf_mt_read_block().
 // We need to split that into a fetch block (compressed) and make this
 // do the actual decompression step.
@@ -1425,10 +1458,16 @@ static int mt_queue(BGZF *fp)
     j->fp = fp;
     j->errcode = 0;
     j->uncomp_len  = fp->block_offset;
-    memcpy(j->uncomp_data, fp->uncompressed_block, j->uncomp_len);
+    if (fp->compress_level == 0) {
+        memcpy(j->comp_data + BLOCK_HEADER_LENGTH + 5, fp->uncompressed_block,
+               j->uncomp_len);
+        hts_tpool_dispatch(mt->pool, mt->out_queue, bgzf_encode_level0_func, j);
+    } else {
+        memcpy(j->uncomp_data, fp->uncompressed_block, j->uncomp_len);
 
-    // Need non-block vers & job_pending?
-    hts_tpool_dispatch(mt->pool, mt->out_queue, bgzf_encode_func, j);
+        // Need non-block vers & job_pending?
+        hts_tpool_dispatch(mt->pool, mt->out_queue, bgzf_encode_func, j);
+    }
 
     fp->block_offset = 0;
     return 0;

commit 5a062a4ea4768b6778c342c4fe5e9c0988b5736b

Fix off by one bug introduced in a13ac99.
Uncompressed BAM files weren't working, which alarmingly also wasn't
visible to the test harness.

We now have checks we can read & write uncompressed BAM and CRAM.

I also noticed a redundant CRAM test of CRAM (default) and CRAM
(version 3).  The default moved to version 3 a while back, so I made
this an explicit check of 2.1 and 3.0.

bgzf.c
@@ -1046,7 +1046,7 @@ void *bgzf_encode_level0_func(void *arg) {
 
     // Add preamble
     memcpy(j->comp_data, g_magic, BLOCK_HEADER_LENGTH);
-    u16_to_le(j->comp_len, j->comp_data + 16);
+    u16_to_le(j->comp_len-1, j->comp_data + 16);
 
     // Deflate uncompressed data header
     j->comp_data[BLOCK_HEADER_LENGTH] = 1; // BFINAL=1, BTYPE=00; see RFC1951

test/test.pl
@@ -276,21 +276,26 @@ sub test_view
         testv $opts, "./test_view $tv_args $bam > $bam.sam_";
         testv $opts, "./compare_sam.pl $sam $bam.sam_";
 
-        # SAM -> CRAM -> SAM
-        testv $opts, "./test_view $tv_args -t $ref -S -C $sam > $cram";
+        # SAM -> BAMu -> SAM
+        testv $opts, "./test_view $tv_args -S -l0 -b $sam > $bam";
+        testv $opts, "./test_view $tv_args $bam > $bam.sam_";
+        testv $opts, "./compare_sam.pl $sam $bam.sam_";
+
+        # SAM -> CRAM2 -> SAM
+        testv $opts, "./test_view $tv_args -t $ref -S -C -o VERSION=2.1 $sam > $cram";
         testv $opts, "./test_view $tv_args -D $cram > $cram.sam_";
         testv $opts, "./compare_sam.pl $md $sam $cram.sam_";
 
-        # BAM -> CRAM -> BAM -> SAM
+        # BAM -> CRAM2 -> BAM -> SAM
         $cram = "$bam.cram";
-        testv $opts, "./test_view $tv_args -t $ref -C $bam > $cram";
+        testv $opts, "./test_view $tv_args -t $ref -C -o VERSION=2.1 $bam > $cram";
         testv $opts, "./test_view $tv_args -b -D $cram > $cram.bam";
         testv $opts, "./test_view $tv_args $cram.bam > $cram.bam.sam_";
         testv $opts, "./compare_sam.pl $md $sam $cram.bam.sam_";
 
-        # SAM -> CRAM3 -> SAM
+        # SAM -> CRAM3u -> SAM
         $cram = "$base.tmp.cram";
-        testv $opts, "./test_view $tv_args -t $ref -S -C -o VERSION=3.0 $sam > $cram";
+        testv $opts, "./test_view $tv_args -t $ref -S -l0 -C -o VERSION=3.0 $sam > $cram";
         testv $opts, "./test_view $tv_args -D $cram > $cram.sam_";
         testv $opts, "./compare_sam.pl $md $sam $cram.sam_";
 