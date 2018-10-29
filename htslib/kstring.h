cb63368c3785d3b11f5e4b3caec9ec71831be044
John Marshall <John.W.Marshall@glasgow.ac.uk>
2018-10-29 22:17:05 +0000

    Make `struct kstring_t` usable without the typedef
    Rename struct tag without reserved-to-the-implementation underscores.
    Header htslib/bgzf.h uses only kstring_t pointers, so can just declare
    the incomplete struct and doesn't need to duplicate the full kstring_t
    definition.
    
    (OTOH htslib/hts.h and htslib/kseq.h do still need the full definition.)

--- htslib/kstring.h
+++ htslib/kstring.h
@@ -70,7 +70,7 @@
  * destroyed with  free(str.s);  */
 #ifndef KSTRING_T
 #define KSTRING_T kstring_t
-typedef struct __kstring_t {
+typedef struct kstring_t {
 	size_t l, m;
 	char *s;
 } kstring_t;
