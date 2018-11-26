874c5bdf80f5b6fac200fd928ac30cb9fd123086
Markus Kusano <>
2018-11-26 17:35:54 -0500

    Use htslib's memory file.
    This removes the code within hts_open_fuzzer creating a temporary file
    and using hts_open's file-based API. Htslib provides an in-memory file
    so obviously we should use this :)

--- test/hts_open_fuzzer.c
+++ test/hts_open_fuzzer.c
@@ -9,42 +9,6 @@
 #include "htslib/sam.h"
 #include "htslib/vcf.h"
 
-static char* fuzzer_get_tmpfile(const uint8_t* data, size_t size) {
-  char* filename_buffer = strdup("/tmp/generate_temporary_file.XXXXXX");
-  if (!filename_buffer) {
-    perror("Failed to allocate file name buffer.");
-    abort();
-  }
-  const int file_descriptor = mkstemp(filename_buffer);
-  if (file_descriptor < 0) {
-    perror("Failed to make temporary file.");
-    abort();
-  }
-  FILE* file = fdopen(file_descriptor, "wb");
-  if (!file) {
-    perror("Failed to open file descriptor.");
-    close(file_descriptor);
-    abort();
-  }
-  const size_t bytes_written = fwrite(data, sizeof(uint8_t), size, file);
-  if (bytes_written < size) {
-    close(file_descriptor);
-    fprintf(stderr, "Failed to write all bytes to file (%zu out of %zu)",
-            bytes_written, size);
-    abort();
-  }
-  fclose(file);
-  return filename_buffer;
-}
-
-static void fuzzer_release_tmpfile(char* filename) {
-  if (unlink(filename) != 0) {
-    perror("WARNING: Failed to delete temporary file.");
-  }
-  free(filename);
-}
-
-
 // Duplicated from: htsfile.c
 static htsFile *dup_stdout(const char *mode) {
   int fd = dup(STDOUT_FILENO);
@@ -52,41 +16,80 @@ static htsFile *dup_stdout(const char *mode) {
   return hfp ? hts_hopen(hfp, "-", mode) : NULL;
 }
 
-static int view_sam(htsFile *in) {
-  if (!in) return 0;
+static void view_sam(htsFile *in) {
+  if (!in) {
+    return;
+  }
   samFile *out = dup_stdout("w");
   bam_hdr_t *hdr = sam_hdr_read(in);
+  if (hdr == NULL) {
+    assert(hts_close(out) == 0 && "Error closing duplicated SAM output descriptor.");
+    return;
+  }
 
-  (void)sam_hdr_write(out, hdr);
+  if (sam_hdr_write(out, hdr) != 0) {
+    bam_hdr_destroy(hdr);
+    assert(hts_close(out) == 0 && "Error closing duplicated SAM output descriptor.");
+    return;
+  }
   bam1_t *b = bam_init1();
-  while (sam_read1(in, hdr, b) >= 0) (void)sam_write1(out, hdr, b);
+  while (sam_read1(in, hdr, b) >= 0) {
+    if (sam_write1(out, hdr, b) < 0) {
+      break;
+    }
+  }
   bam_destroy1(b);
 
   bam_hdr_destroy(hdr);
-  hts_close(out);
-  return 1;
+  assert(hts_close(out) == 0 && "Error closing duplicated SAM output descriptor.");
+  return;
 }
 
-static int view_vcf(htsFile *in) {
-  if (!in) return 0;
+static void view_vcf(htsFile *in) {
+  if (!in) {
+    return;
+  }
   vcfFile *out = dup_stdout("w");
   bcf_hdr_t *hdr = bcf_hdr_read(in);
+  if (hdr == NULL) {
+    assert(hts_close(out) == 0 && "Error closing duplicated VCF output descriptor.");
+    return;
+  }
 
-  bcf_hdr_write(out, hdr);
+  if (bcf_hdr_write(out, hdr) != 0) {
+    bcf_hdr_destroy(hdr);
+    assert(hts_close(out) == 0 && "Error closing duplicated VCF output descriptor.");
+  }
   bcf1_t *rec = bcf_init();
-  while (bcf_read(in, hdr, rec) >= 0) bcf_write(out, hdr, rec);
+  while (bcf_read(in, hdr, rec) >= 0) {
+    if (bcf_write(out, hdr, rec) < 0) {
+      break;
+    }
+  }
   bcf_destroy(rec);
 
   bcf_hdr_destroy(hdr);
-  hts_close(out);
-  return 1;
+  assert(hts_close(out) == 0 && "Error closing duplicated VCF output descriptor.");
+  return;
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  char* temporary_file = fuzzer_get_tmpfile(data, size);
-  htsFile *ht_file = hts_open(temporary_file, /*mode=*/"r");
+  hFILE *memfile;
+  uint8_t *copy;
+  copy = malloc(size);
+  if (copy == NULL) {
+    return 0;
+  }
+  memcpy(copy, data, size);
+  memfile = hopen("mem:", "rb:", copy, size);
+  if (memfile == NULL) {
+    free(copy);
+    return 0;
+  }
+
+  htsFile *ht_file = hts_hopen(memfile, "data", "rb");
   if (ht_file == NULL) {
-    fuzzer_release_tmpfile(temporary_file);
+    assert(hclose(memfile) == 0 && "Error closing memfile.");
     return 0;
   }
   switch (ht_file->format.category) {
@@ -100,6 +103,5 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
       break;
   }
   hts_close(ht_file);
-  fuzzer_release_tmpfile(temporary_file);
   return 0;
 }
