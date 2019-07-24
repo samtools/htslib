f52abaf97cd013697e20bae621df55641ebaa19a
John Marshall <John.W.Marshall@glasgow.ac.uk>
2019-07-24 13:22:20 +0100

    Enlarge bam1_core_t::l_qname to allow values 0..256 inclusive
    So that CIGAR data is 32-bit aligned, there are extra padding NULs
    in memory after qname (see 5d114ebd8e9b80622769b8e575c5a9359cd51273).
    For simplicity in the bam_get_cigar() et al macros, these NULs are
    counted in both l_qname and l_extranul -- but this means that the
    maximum value for l_qname is 256 (namely a QNAME of length >= 252,
    plus one NUL terminator, plus more NULs to get to a multiple of 4).
    
    Make l_qname a uint16_t and (to retain the simplicity) leave the
    l_qname/l_extranul representation as it is. Rearrange the two fields
    so that the larger field takes over the previous unused1's space, so
    that sizeof(bam1_core_t) is unchanged.
    
    Revert the checks added by 079c7b81d3eb740f575d6ee07f8d0df36932559e,
    which are no longer needed. In bam_construct_seq(), just trust the
    length obtained from the CRAM record; in bam_read1(), the l_qname
    value has come from a 1-byte raw field, so there is no longer any
    overflow risk; in sam_parse1(), revert the check to check against
    the SAM spec's exact 254 maximum length (+ 1 for the following TAB).
    
    Add test cases with long read names.
    
    Fixes #520.

--- sam.c
+++ sam.c
@@ -545,8 +545,6 @@ int bam_read1(BGZF *fp, bam1_t *b)
     c->tid = x[0]; c->pos = x[1];
     c->bin = x[2]>>16; c->qual = x[2]>>8&0xff; c->l_qname = x[2]&0xff;
     c->l_extranul = (c->l_qname%4 != 0)? (4 - c->l_qname%4) : 0;
-    if ((uint32_t) c->l_qname + c->l_extranul > 255) // l_qname would overflow
-        return -4;
     c->flag = x[3]>>16; c->n_cigar = x[3]&0xffff;
     c->l_qseq = x[4];
     c->mtid = x[5]; c->mpos = x[6]; c->isize = x[7];
@@ -1662,7 +1660,7 @@ int sam_parse1(kstring_t *s, sam_hdr_t *h, bam1_t *b)
     q = _read_token(p);
 
     _parse_warn(p - q <= 1, "empty query name");
-    _parse_err(p - q > 252, "query name too long");
+    _parse_err(p - q > 255, "query name too long");
     // resize large enough for name + extranul
     if ((p-q)+4 > SIZE_MAX - s->l || ks_resize(&str, str.l+(p-q)+4) < 0) goto err_ret;
     memcpy(str.s+str.l, q, p-q); str.l += p-q;
