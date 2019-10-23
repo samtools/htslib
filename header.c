442b28d94c6a2aba7d72d886e5de89b3a657ade5
James <james@softgenetics.com>
2019-10-23 17:46:23 -0400

    add check for invalid tid < 0 and return null

--- header.c
+++ header.c
@@ -1944,7 +1944,7 @@ int sam_hdr_name2tid(sam_hdr_t *bh, const char *ref) {
 const char *sam_hdr_tid2name(const sam_hdr_t *h, int tid) {
     sam_hrecs_t *hrecs;
 
-    if (!h)
+    if (!h || tid < 0)
         return NULL;
 
     if ((hrecs = h->hrecs) != NULL && tid < hrecs->nref) {
