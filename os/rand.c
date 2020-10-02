3db2143e9ef5dc11237ace71a7ad69694992cc94
Anders Kaplan <anders.kaplan@magasin1.se>
2020-10-02 11:02:51 +0200

    Added missing include to os/rand.c (for HTSLIB_EXPORT).

--- os/rand.c
+++ os/rand.c
@@ -21,6 +21,7 @@
 //__FBSDID("$FreeBSD: src/lib/libc/gen/_rand48.c,v 1.2 2002/03/22 21:52:05 obrien Exp $");
 
 #include <math.h>
+#include "htslib/hts_defs.h"
 
 #define	RAND48_SEED_0	(0x330e)
 #define	RAND48_SEED_1	(0xabcd)
