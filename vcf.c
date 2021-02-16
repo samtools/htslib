8127bfc98e9b4361dca2423fd42a59ad7c25dda7
Rob Davies <rmd+git@sanger.ac.uk>
2021-02-16 16:49:59 +0000

    Fix reading UTF-8 encoded sample names when char is signed
    The trick used in bcf_hdr_parse_sample_line() to rapidly find tabs
    and newlines could be defeated by UTF-8 characters outside the
    Basic Latin range on platforms where "char" is signed (like x86).
    It's currently not clear if VCF intends to allow these, but the
    4.3 specification does allow UTF-8 and it's easy enough to support.
    Fix by casting to unsigned when making the comparison.
    
    Modifies formatcols.vcf to include a UTF-8 character for a
    round-trip test.
    
    Fixes samtools/bcftools#1408

--- vcf.c
+++ vcf.c
@@ -150,7 +150,7 @@ int HTS_RESULT_USED bcf_hdr_parse_sample_line(bcf_hdr_t *h, const char *str)
     const char *p, *q;
     // add samples
     for (p = q = str;; ++q) {
-        if (*q > '\n') continue;
+        if ((unsigned char) *q > '\n') continue;
         if (++i > 9) {
             if ( bcf_hdr_add_sample_len(h, p, q - p) < 0 ) ret = -1;
         }
