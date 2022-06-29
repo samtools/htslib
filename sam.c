5291622b08381a47eb3941b11705105c841e69b7
Rob Davies <rmd+git@sanger.ac.uk>
2022-06-29 17:35:46 +0100

    Deprecate hts_filter_eval() in favour of hts_filter_eval2().
    Due to hts_filter_eval() calling memset() on its res parameter,
    it's not possible to pass in an allocted kstring_t in res->s
    without leaking memory.  Historically it was also possible
    to get away with passing in an uninitialised structure, so not
    many assumptions can be made about the contents of res on entry.
    In particular, it is not guaranteed that free(res->s.s) would
    work.
    
    To ensure the function is being used safely, check that the
    string part of *res is NULL on entry and fail if not.
    Also added a documentation note about calling hts_expr_val_free()
    after hts_filter_eval().
    
    Add hts_filter_eval2() and deprecate hts_filter_eval().  The new
    function clears its `res` parameter properly, allowing it to
    be called repeatedly a bit more easily than the original.

--- sam.c
+++ sam.c
@@ -1454,8 +1454,8 @@ static int bam_sym_lookup(void *data, char *str, char **end,
 int sam_passes_filter(const sam_hdr_t *h, const bam1_t *b, hts_filter_t *filt)
 {
     hb_pair hb = {h, b};
-    hts_expr_val_t res;
-    if (hts_filter_eval(filt, &hb, bam_sym_lookup, &res)) {
+    hts_expr_val_t res = HTS_EXPR_VAL_INIT;
+    if (hts_filter_eval2(filt, &hb, bam_sym_lookup, &res)) {
         hts_log_error("Couldn't process filter expression");
         hts_expr_val_free(&res);
         return -1;
