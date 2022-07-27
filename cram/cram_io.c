samtools/htslib

commit 9562aebaa39265b7c96e6a6f5cf6f78727c41221

Improve embed_ref=2 and auto-enable it when missing external refs (PR #1449)
* Improve embed_ref=2 when no MD:Z tags are present.

When these tags are present, the reference is inferred so it's much
the same as before.

However when not present, we previously took a first-come first-serve
approach to consensus generation as the just-in-time reference
generation was in the process_one_read loop.  As we'd already updated
the reference and delta-encoded against it, if a later read comes
along with conflicts then we couldn't correct anything.

33ff2bc incorrectly made the assumption that we were being fed a bunch
of bam records and didn't have all the data available up-front.
However this isn't true.  For the sake of threading efficiency, the
encoder buffers up all the BAMs for a container before dispatching the
cram_encode_container call, so we can do a consensus generation stage
prior to the process_one_read loop.  This improves consensus accuracy
and also simplifies the MD:Z using code too as it doesn't have to
worry about inconsistencies any more.

On 10MB worth of ONT data we had the following total file and SEQ sizes:

                   Total      SEQ only
    no_ref         205899946  74416907
    embed_ref=0    135303919  27583376  use external ref
    embed_ref=1    139709759  32008185  embed external ref, from file
    embed_ref=2+MD 139710386  32007405  embed external ref, inferred
    embed_ref=2-MD 149371457  41670856  OLD: first-come first-serve cons
    embed_ref=2-MD 139598781  31898253  NEW: proper consensus

The old embed_ref=2 needed 30% more space to delta-encode seqs,
although it was still a major win over using no_ref mode.  The new
consensus is is a significant improvement and it's now a better match
for the sequence (as we'd expect) than the external reference.
Obviously this can't be done when we have MD tags as otherwise we'd
need to store them verbatim which then takes more space up again.

cram/cram_io.c
@@ -3561,7 +3561,7 @@ int cram_load_reference(cram_fd *fd, char *fn) {
 
     if (fn) {
         fd->refs = refs_load_fai(fd->refs, fn,
-                                 !(fd->embed_ref && fd->mode == 'r'));
+                                 !(fd->embed_ref>0 && fd->mode == 'r'));
         fn = fd->refs ? fd->refs->fn : NULL;
         if (!fn)
             ret = -1;
@@ -3712,10 +3712,8 @@ void cram_free_container(cram_container *c) {
         kh_destroy(m_tagmap, c->tags_used);
     }
 
-    if (c->ref_free) {
+    if (c->ref_free)
         free(c->ref);
-        free(c->ref_set);
-    }
 
     free(c);
 }
@@ -4850,7 +4848,21 @@ int cram_write_SAM_hdr(cram_fd *fd, sam_hdr_t *hdr) {
                 if (!(md5 = hts_md5_init()))
                     return -1;
                 ref = cram_get_ref(fd, i, 1, rlen);
-                if (NULL == ref) return -1;
+                if (NULL == ref) {
+                    if (fd->embed_ref == -1) {
+                        // auto embed-ref
+                        hts_log_warning("No M5 tags present and could not "
+                                        "find reference");
+                        hts_log_warning("Enabling embed_ref=2 option");
+                        hts_log_warning("NOTE: the CRAM file will be bigger "
+                                        "than using an external reference");
+                        pthread_mutex_lock(&fd->ref_lock);
+                        fd->embed_ref = 2;
+                        pthread_mutex_lock(&fd->ref_lock);
+                        break;
+                    }
+                    return -1;
+                }
                 rlen = fd->refs->ref_id[i]->length; /* In case it just loaded */
                 hts_md5_update(md5, ref, rlen);
                 hts_md5_final(buf, md5);
@@ -5251,7 +5263,7 @@ cram_fd *cram_dopen(hFILE *fp, const char *filename, const char *mode) {
     fd->seqs_per_slice = SEQS_PER_SLICE;
     fd->bases_per_slice = BASES_PER_SLICE;
     fd->slices_per_container = SLICE_PER_CNT;
-    fd->embed_ref = 0;
+    fd->embed_ref = -1; // automatic selection
     fd->no_ref = 0;
     fd->ap_delta = 0;
     fd->ignore_md5 = 0;
@@ -5398,7 +5410,7 @@ int cram_write_eof_block(cram_fd *fd) {
         //   block CRC
         cram_block_compression_hdr ch;
         memset(&ch, 0, sizeof(ch));
-        c.comp_hdr_block = cram_encode_compression_header(fd, &c, &ch);
+        c.comp_hdr_block = cram_encode_compression_header(fd, &c, &ch, 0);
 
         c.length = c.comp_hdr_block->byte            // Landmark[0]
             + 5                                      // block struct

Makefile
@@ -447,7 +447,7 @@ textutils.o textutils.pico: textutils.c config.h $(htslib_hfile_h) $(htslib_kstr
 
 cram/cram_codecs.o cram/cram_codecs.pico: cram/cram_codecs.c config.h $(htslib_hts_endian_h) $(htscodecs_varint_h) $(htscodecs_pack_h) $(htscodecs_rle_h) $(cram_h)
 cram/cram_decode.o cram/cram_decode.pico: cram/cram_decode.c config.h $(cram_h) $(cram_os_h) $(htslib_hts_h)
-cram/cram_encode.o cram/cram_encode.pico: cram/cram_encode.c config.h $(cram_h) $(cram_os_h) $(sam_internal_h) $(htslib_hts_h) $(htslib_hts_endian_h)
+cram/cram_encode.o cram/cram_encode.pico: cram/cram_encode.c config.h $(cram_h) $(cram_os_h) $(sam_internal_h) $(htslib_hts_h) $(htslib_hts_endian_h) $(textutils_internal_h)
 cram/cram_external.o cram/cram_external.pico: cram/cram_external.c config.h $(htslib_hfile_h) $(cram_h)
 cram/cram_index.o cram/cram_index.pico: cram/cram_index.c config.h $(htslib_bgzf_h) $(htslib_hfile_h) $(hts_internal_h) $(cram_h) $(cram_os_h)
 cram/cram_io.o cram/cram_io.pico: cram/cram_io.c config.h os/lzma_stub.h $(cram_h) $(cram_os_h) $(htslib_hts_h) $(cram_open_trace_file_h) $(htscodecs_rANS_static_h) $(htscodecs_rANS_static4x16_h) $(htscodecs_arith_dynamic_h) $(htscodecs_tokenise_name3_h) $(htscodecs_fqzcomp_qual_h) $(htscodecs_varint_h) $(htslib_hfile_h) $(htslib_bgzf_h) $(htslib_faidx_h) $(hts_internal_h)

cram/cram_encode.h
@@ -74,7 +74,8 @@ int cram_put_bam_seq(cram_fd *fd, bam_seq_t *b);
  *         NULL on failure
  */
 cram_block *cram_encode_compression_header(cram_fd *fd, cram_container *c,
-                                           cram_block_compression_hdr *h);
+                                           cram_block_compression_hdr *h,
+                                           int embed_ref);
 
 /*! INTERNAL:
  * Encodes a slice compression header.

cram/cram_external.c
@@ -331,7 +331,7 @@ int cram_transcode_rg(cram_fd *in, cram_fd *out,
         return -1;
     if (cram_block_compression_hdr_decoder2encoder(in, ch) != 0)
         return -1;
-    n_blk = cram_encode_compression_header(in, c, ch);
+    n_blk = cram_encode_compression_header(in, c, ch, in->embed_ref);
     cram_free_compression_header(ch);
 
     /*

cram/cram_structs.h
@@ -474,7 +474,6 @@ struct cram_container {
 
     uint32_t n_mapped;    // Number of mapped reads
     int ref_free;         // whether 'ref' is owned by us and must be freed.
-    char *ref_set;        // same size as ref.  Only set for auto embed_ref
 };
 
 /*

commit 061e968438334d3239deb9b473af606fa325786a

Fix (un)locking bug introduced in commit 9562aeba

cram/cram_io.c
@@ -4858,7 +4858,7 @@ int cram_write_SAM_hdr(cram_fd *fd, sam_hdr_t *hdr) {
                                         "than using an external reference");
                         pthread_mutex_lock(&fd->ref_lock);
                         fd->embed_ref = 2;
-                        pthread_mutex_lock(&fd->ref_lock);
+                        pthread_mutex_unlock(&fd->ref_lock);
                         break;
                     }
                     return -1;