7bfa464a56284f8f96abd169d5f54649edb69d44
James Bonfield <jkb@sanger.ac.uk>
2023-11-16 17:30:24 +0000

    Improve the fuzzer to write BAM/CRAM and BCF too.
    This tests many more code paths (encoding).

--- test/fuzz/hts_open_fuzzer.c
+++ test/fuzz/hts_open_fuzzer.c
@@ -47,73 +47,103 @@ static void view_sam(htsFile *in) {
     if (!in) {
         return;
     }
-    samFile *out = sam_open("/dev/null", "w");
-    if (!out) {
+    samFile *outS = sam_open("/dev/null", "w");
+    samFile *outB = sam_open("/dev/null", "wb");
+    samFile *outC = sam_open("/dev/null", "wc");
+    if (!outS || !outB || !outC) {
         abort();
+    }
+    // Not critical if this doesn't work, but can test more if
+    // we're in the right location.
+    if (hts_set_fai_filename(outC, "../c2.fa") < 0) {
+        static int warned = 0;
+        if (!warned) {
+            warned = 1;
+            fprintf(stderr, "Warning couldn't find the c2.fa file\n");
+        }
     }
     sam_hdr_t *hdr = sam_hdr_read(in);
     if (hdr == NULL) {
-        hts_close_or_abort(out);
+        hts_close_or_abort(outS);
+        hts_close_or_abort(outB);
+        hts_close(outC);
         return;
     }
 
     // This will force the header to be parsed.
     (void) sam_hdr_count_lines(hdr, "SQ");
 
-    if (sam_hdr_write(out, hdr) != 0) {
+    if (sam_hdr_write(outS, hdr) != 0 ||
+        sam_hdr_write(outB, hdr) != 0 ||
+        sam_hdr_write(outC, hdr) != 0) {
         sam_hdr_destroy(hdr);
-        hts_close_or_abort(out);
+        hts_close_or_abort(outS);
+        hts_close_or_abort(outB);
+        hts_close(outC);
         return;
     }
     bam1_t *b = bam_init1();
     if (b == NULL) {
         sam_hdr_destroy(hdr);
-        hts_close_or_abort(out);
+        hts_close_or_abort(outS);
+        hts_close_or_abort(outB);
+        hts_close(outC);
         return;
     }
     while (sam_read1(in, hdr, b) >= 0) {
-        if (sam_write1(out, hdr, b) < 0) {
+        if (sam_write1(outS, hdr, b) < 0 ||
+            sam_write1(outB, hdr, b) < 0 ||
+            sam_write1(outC, hdr, b) < 0) {
             break;
         }
     }
     bam_destroy1(b);
 
     sam_hdr_destroy(hdr);
-    hts_close_or_abort(out);
+    hts_close_or_abort(outS);
+    hts_close_or_abort(outB);
+    hts_close(outC);
 }
 
 static void view_vcf(htsFile *in) {
     if (!in) {
         return;
     }
-    vcfFile *out = vcf_open("/dev/null", "w");
-    if (!out) {
+    vcfFile *outv = vcf_open("/dev/null", "w");
+    vcfFile *outb = vcf_open("/dev/null", "wb");
+    if (!outv || !outb) {
         abort();
     }
     bcf_hdr_t *hdr = bcf_hdr_read(in);
     if (hdr == NULL) {
-        hts_close_or_abort(out);
+        hts_close_or_abort(outv);
+        hts_close_or_abort(outb);
         return;
     }
 
-    if (bcf_hdr_write(out, hdr) != 0) {
+    if (bcf_hdr_write(outv, hdr) != 0 ||
+        bcf_hdr_write(outb, hdr) != 0) {
         bcf_hdr_destroy(hdr);
-        hts_close_or_abort(out);
+        hts_close_or_abort(outv);
+        hts_close_or_abort(outb);
     }
     bcf1_t *rec = bcf_init();
     if (rec == NULL) {
         bcf_hdr_destroy(hdr);
-        hts_close_or_abort(out);
+        hts_close_or_abort(outv);
+        hts_close_or_abort(outb);
     }
     while (bcf_read(in, hdr, rec) >= 0) {
-        if (bcf_write(out, hdr, rec) < 0) {
+        if (bcf_write(outv, hdr, rec) < 0 ||
+            bcf_write(outb, hdr, rec) < 0) {
             break;
         }
     }
     bcf_destroy(rec);
 
     bcf_hdr_destroy(hdr);
-    hts_close_or_abort(out);
+    hts_close_or_abort(outv);
+    hts_close_or_abort(outb);
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
