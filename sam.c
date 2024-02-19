d028e0d844af96a536c1e2722337a9d5a06a8255
James Bonfield <jkb@sanger.ac.uk>
2024-02-19 17:30:30 +0000

    Call the pileup destructor in bam_plp_destroy.
    This frees memory when destroying earlier than expected, such as
    during a processing failure.
    
    I can't figure out how this has been missed all these years!

--- sam.c
+++ sam.c
@@ -5560,6 +5560,8 @@ void bam_plp_destroy(bam_plp_t iter)
     lbnode_t *p, *pnext;
     if ( iter->overlaps ) kh_destroy(olap_hash, iter->overlaps);
     for (p = iter->head; p != NULL; p = pnext) {
+        if (iter->plp_destruct)
+            iter->plp_destruct(iter->data, &p->b, &p->cd);
         pnext = p->next;
         mp_free(iter->mp, p);
     }
