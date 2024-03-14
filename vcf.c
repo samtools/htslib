samtools/htslib

commit b49eea478114759efe19e718c38e7deb00614b21

VCF parsing fix
The removal of large tags by d800f1323a545bfcf2bf8fe3f63b6e430bb5e75d inadvertently
changed output for invalid rows, such as the one introduced by 8f782d120433eb0321a1c0db6ee49e6bc838563e.
This was caused by relying on fmt->size==0 never happening - such assumption is
correct, I think, but only for valid tags, not such that failed parsing

vcf.c
@@ -3008,7 +3008,8 @@ static int vcf_parse_format_alloc4(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
             if ( !warned ) hts_log_warning("Excessive memory required by FORMAT fields at %s:%"PRIhts_pos, bcf_seqname_safe(h,v), v->pos+1);
             warned = 1;
             v->errcode |= BCF_ERR_LIMITS;
-            f->size = f->offset = 0;
+            f->size = -1;
+            f->offset = 0;
             continue;
         }
 
@@ -3068,7 +3069,7 @@ static int vcf_parse_format_fill5(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
                 return -1;
             }
 
-            if ( !z->size )
+            if ( z->size==-1 )
             {
                 // this field is to be ignored, it's too big
                 while ( *t != ':' && *t ) t++;
@@ -3249,7 +3250,7 @@ static int vcf_parse_format_gt6(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
     if (v->n_sample > 0) {
         for (i = 0; i < v->n_fmt; ++i) {
             fmt_aux_t *z = &fmt[i];
-            if ( !z->size ) {
+            if ( z->size==-1 ) {
                 need_downsize = 1;
                 continue;
             }
@@ -3274,7 +3275,7 @@ static int vcf_parse_format_gt6(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
     if ( need_downsize ) {
         i = 1;
         while ( i < v->n_fmt ) {
-            if ( !fmt[i].size )
+            if ( fmt[i].size==-1 )
             {
                 memmove(&fmt[i-1],&fmt[i],sizeof(*fmt));
                 v->n_fmt--;

commit c76e487a9a642b484db4845bcc57b7d69cbf824f

Drop duplicate FORMAT tags and fix an error with dropping large tags
The removal of large tags introduced by b49eea4 and 9db565de could
not work correctly, the memmove pointers were wrong!

Resolves #1733

vcf.c
@@ -1,7 +1,7 @@
 /*  vcf.c -- VCF/BCF API functions.
 
     Copyright (C) 2012, 2013 Broad Institute.
-    Copyright (C) 2012-2023 Genome Research Ltd.
+    Copyright (C) 2012-2024 Genome Research Ltd.
     Portions copyright (C) 2014 Intel Corporation.
 
     Author: Heng Li <lh3@sanger.ac.uk>
@@ -3029,6 +3029,26 @@ static int vcf_parse_format_alloc4(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
             fmt[j].buf = (uint8_t*)mem->s + fmt[j].offset;
     }
 
+    // check for duplicate tags
+    int i;
+    for (i=1; i<v->n_fmt; i++)
+    {
+        fmt_aux_t *ifmt = &fmt[i];
+        if ( ifmt->size==-1 ) continue; // already marked for removal
+        for (j=0; j<i; j++)
+        {
+            fmt_aux_t *jfmt = &fmt[j];
+            if ( jfmt->size==-1 ) continue; // already marked for removal
+            if ( ifmt->key!=jfmt->key ) continue;
+            static int warned = 0;
+            if ( !warned ) hts_log_warning("Duplicate FORMAT tag %s at %s:%"PRIhts_pos, bcf_hdr_int2id(h,BCF_DT_ID,ifmt->key), bcf_seqname_safe(h,v), v->pos+1);
+            warned = 1;
+            v->errcode |= BCF_ERR_TAG_INVALID;
+            ifmt->size = -1;
+            ifmt->offset = 0;
+            break;
+        }
+    }
     return 0;
 }
 
@@ -3072,7 +3092,7 @@ static int vcf_parse_format_fill5(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
 
             if ( z->size==-1 )
             {
-                // this field is to be ignored, it's too big
+                // this field is to be ignored, it's either too big or a duplicate
                 while ( *t != ':' && *t ) t++;
             }
             else if (htype == BCF_HT_STR) {
@@ -3274,18 +3294,17 @@ static int vcf_parse_format_gt6(kstring_t *s, const bcf_hdr_t *h, bcf1_t *v,
 
     }
     if ( need_downsize ) {
-        i = 1;
+        i = 0;
         while ( i < v->n_fmt ) {
             if ( fmt[i].size==-1 )
             {
-                memmove(&fmt[i-1],&fmt[i],sizeof(*fmt));
                 v->n_fmt--;
+                if ( i < v->n_fmt ) memmove(&fmt[i],&fmt[i+1],sizeof(*fmt)*(v->n_fmt-i));
             }
             else
                 i++;
         }
     }
-
     return 0;
 }
 