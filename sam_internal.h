be6a8fb5a0dc00089d0c507fc1227c1c986444d6
John Marshall <jmarshall@hey.com>
2024-06-23 22:05:59 +1200

    Add ARM Neon nibble2base_neon() implementation
    As ARM CPU capability instructions are privileged, we also need to
    add cpu_supports_neon() and implement it to query for Neon/AdvSIMD
    in various platform-dependent ways. (On 32-bit ARM, glibc helpfully
    renamed the HWCAP_* macros to HWCAP_ARM_*, so accept both on Linux.)
    
    32-bit ARM GCC erroneously does not define vst1q_u8_x2() (bug 71233,
    fixed in v14.1 in this case), so avoid it on 32-bit ARM by writing
    interleaved via vst2q_u8() instead.

--- sam_internal.h
+++ sam_internal.h
@@ -100,7 +100,8 @@ static inline void nibble2base_default(uint8_t *nib, char *seq, int len) {
 }
 
 #if defined HAVE_ATTRIBUTE_CONSTRUCTOR && \
-    defined __x86_64__ && defined HAVE_ATTRIBUTE_TARGET && defined HAVE_BUILTIN_CPU_SUPPORT_SSSE3
+    ((defined __x86_64__ && defined HAVE_ATTRIBUTE_TARGET && defined HAVE_BUILTIN_CPU_SUPPORT_SSSE3) || \
+     (defined __ARM_NEON))
 #define BUILDING_SIMD_NIBBLE2BASE
 #endif
 
