567616b1c15c6574e76b0a05b53a0406f6b8e4fb
pd3 <pd3@sanger.ac.uk>
2024-11-17 20:30:03 +0100

    Fix a bug in breakend detection
    it was incorrectly assumed that the ALT allele is
    of equal length to REF allele, but the VCF specification
    allows breakend insertions, such as C > CAGTNNNNNCA[2:321682[
    
    Resolves https://github.com/samtools/bcftools/issues/2317

--- vcf.c
+++ vcf.c
@@ -5025,8 +5025,8 @@ static void bcf_set_variant_type(const char *ref, const char *alt, bcf_variant_t
 
     if ( *a && !*r )
     {
-        if ( *a==']' || *a=='[' ) { var->type = VCF_BND; return; } // "joined after" breakend
         while ( *a ) a++;
+        if ( *(a-1)==']' || *(a-1)=='[' ) { var->type = VCF_BND; return; } // "joined after" breakend
         var->n = (a-alt)-(r-ref); var->type = VCF_INDEL | VCF_INS; return;
     }
     else if ( *r && !*a )
