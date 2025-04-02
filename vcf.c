f2a3b5728972da488a0a6bbd81e1b770ca787db2
Petr Danecek <pd3@sanger.ac.uk>
2025-04-02 08:46:54 +0100

    Recognize T > A[chr15:12345[ breakend type

--- vcf.c
+++ vcf.c
@@ -5142,17 +5142,20 @@ static void bcf_set_variant_type(const char *ref, const char *alt, bcf_variant_t
     }
     else if ( *r && !*a )
     {
+        if ( *(a-1)==']' || *(a-1)=='[' ) { var->type = VCF_BND; return; } // "joined after" breakend
         while ( *r ) r++;
         var->n = (a-alt)-(r-ref); var->type = VCF_INDEL | VCF_DEL; return;
     }
     else if ( !*r && !*a )
     {
+        if ( *(a-1)==']' || *(a-1)=='[' ) { var->type = VCF_BND; return; } // "joined after" breakend
         var->n = 0; var->type = VCF_REF; return;
     }
 
     const char *re = r, *ae = a;
     while ( re[1] ) re++;
     while ( ae[1] ) ae++;
+    if ( ae[0]==']' || ae[0]=='[' ) { var->type = VCF_BND; return; }    // "joined after" breakend
     while ( re>r && ae>a && toupper_c(*re)==toupper_c(*ae) ) { re--; ae--; }
     if ( ae==a )
     {
