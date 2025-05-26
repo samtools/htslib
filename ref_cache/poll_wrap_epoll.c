62ad3ddad8238bc5e9d304331056cbc4a8be8af9
Rob Davies <rmd+git@sanger.ac.uk>
2025-05-26 22:33:28 +0100

    Fix indentation -- USE -b/-w TO DIFF/BLAME ACROSS THIS COMMIT
    Unfortunately poll_wrap_epoll was missed when importing to
    HTSlib.

--- ref_cache/poll_wrap_epoll.c
+++ ref_cache/poll_wrap_epoll.c
@@ -41,104 +41,104 @@ DEALINGS IN THE SOFTWARE.  */
 #define INIT_EPOLL_SIZE 128
 
 struct Poll_wrap {
-  pool_alloc_t *pool;
-  int epfd;
-  int debug;
+    pool_alloc_t *pool;
+    int epfd;
+    int debug;
 };
 
 Poll_wrap *pw_init(int debug) {
-  Poll_wrap *pw = calloc(1, sizeof(Poll_wrap));
-  if (pw == NULL) return NULL;
+    Poll_wrap *pw = calloc(1, sizeof(Poll_wrap));
+    if (pw == NULL) return NULL;
 
-  pw->pool = pool_create(sizeof(Pw_item));
-  if (pw->pool == NULL) {
-    free(pw);
-    return NULL;
-  }
+    pw->pool = pool_create(sizeof(Pw_item));
+    if (pw->pool == NULL) {
+        free(pw);
+        return NULL;
+    }
 
-  pw->epfd = epoll_create(INIT_EPOLL_SIZE);
-  if (pw->epfd < 0) {
-    perror("epoll_create");
-    pool_destroy(pw->pool);
-    free(pw);
-    return NULL;
-  }
+    pw->epfd = epoll_create(INIT_EPOLL_SIZE);
+    if (pw->epfd < 0) {
+        perror("epoll_create");
+        pool_destroy(pw->pool);
+        free(pw);
+        return NULL;
+    }
 
-  pw->debug = debug;
+    pw->debug = debug;
 
-  return pw;
+    return pw;
 }
 
 void pw_close(Poll_wrap *pw) {
-  close(pw->epfd);
-  pool_destroy(pw->pool);
-  free(pw);
+    close(pw->epfd);
+    pool_destroy(pw->pool);
+    free(pw);
 }
 
 Pw_item * pw_register(Poll_wrap *pw, int fd, Pw_fd_type fd_type,
                       uint32_t init_events, void *userp) {
-  struct epoll_event event;
-  Pw_item *item = pool_alloc(pw->pool);
-  if (item == NULL)
-      return NULL;
+    struct epoll_event event;
+    Pw_item *item = pool_alloc(pw->pool);
+    if (item == NULL)
+        return NULL;
 
-  if (pw->debug) {
-      fprintf(stderr, "pw_register(%p, %d, %d, 0x%04x, %p)\n",
-              (void *) pw, fd, (int) fd_type, init_events, userp);
-  }
+    if (pw->debug) {
+        fprintf(stderr, "pw_register(%p, %d, %d, 0x%04x, %p)\n",
+                (void *) pw, fd, (int) fd_type, init_events, userp);
+    }
 
-  item->fd      = fd;
-  item->fd_type = fd_type;
-  item->userp   = userp;
+    item->fd      = fd;
+    item->fd_type = fd_type;
+    item->userp   = userp;
 
-  event.events = init_events;
-  event.data.ptr = item;
+    event.events = init_events;
+    event.data.ptr = item;
 
-  if (epoll_ctl(pw->epfd, EPOLL_CTL_ADD, fd, &event) != 0) {
-    perror("epoll_ctl");
-    pool_free(pw->pool, item);
-    return NULL;
-  }
+    if (epoll_ctl(pw->epfd, EPOLL_CTL_ADD, fd, &event) != 0) {
+        perror("epoll_ctl");
+        pool_free(pw->pool, item);
+        return NULL;
+    }
 
-  return item;
+    return item;
 }
 
 int pw_mod(Poll_wrap *pw, Pw_item *item, uint32_t events) {
-  struct epoll_event event;
+    struct epoll_event event;
 
-  if (pw->debug) {
-      fprintf(stderr, "pw_mod(%p, %d, 0x%04x)\n",
-              (void *) pw, item->fd, events);
-  }
+    if (pw->debug) {
+        fprintf(stderr, "pw_mod(%p, %d, 0x%04x)\n",
+                (void *) pw, item->fd, events);
+    }
 
-  event.events   = events;
-  event.data.ptr = item;
+    event.events   = events;
+    event.data.ptr = item;
 
-  return epoll_ctl(pw->epfd, EPOLL_CTL_MOD, item->fd, &event);
+    return epoll_ctl(pw->epfd, EPOLL_CTL_MOD, item->fd, &event);
 }
 
 int pw_wait(Poll_wrap *pw, Pw_events *events,
-        int max_events, int timeout) {
-  return epoll_wait(pw->epfd, (struct epoll_event *) events,
-            max_events, timeout);
+            int max_events, int timeout) {
+    return epoll_wait(pw->epfd, (struct epoll_event *) events,
+                      max_events, timeout);
 }
 
 int pw_remove(Poll_wrap *pw, Pw_item *item, int do_close) {
-  struct epoll_event dummy;
-  int res;
+    struct epoll_event dummy;
+    int res;
 
-  if (pw->debug) {
-      fprintf(stderr, "pw_remove(%p, %d%s)\n",
-              (void *) pw, item->fd, do_close ? ", close" : "");
-  }
+    if (pw->debug) {
+        fprintf(stderr, "pw_remove(%p, %d%s)\n",
+                (void *) pw, item->fd, do_close ? ", close" : "");
+    }
 
-  if (do_close) {
-    res = close(item->fd);
-  } else {
-    res = epoll_ctl(pw->epfd, EPOLL_CTL_DEL, item->fd, &dummy);
-  }
-  pool_free(pw->pool, item);
-  return res;
+    if (do_close) {
+        res = close(item->fd);
+    } else {
+        res = epoll_ctl(pw->epfd, EPOLL_CTL_DEL, item->fd, &dummy);
+    }
+    pool_free(pw->pool, item);
+    return res;
 }
 #else
 // Prevent "empty translation unit" errors
