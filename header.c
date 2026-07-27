7a4f4c1cbcb0fc36f98796beb7c415cc3868d7e7
Andrew Whitwham <aw7@sanger.ac.uk>
2026-07-27 12:30:25 +0100

    Removed a redundant loop.

--- header.c
+++ header.c
@@ -3206,17 +3206,12 @@ int sam_hdr_remove_tag_all(sam_hdr_t *bh, const char *type, const char *tag_id) 
     t1 = t2 = kh_val(hrecs->h, k);
 
     do {
-        sam_hrec_tag_t *tag;
-        for (tag = t1->tag; tag; tag = tag->next) {
-            if (tag->str[0] == tag_id[0] && tag->str[1] == tag_id[1]) {
-                if ((ret = sam_hrecs_remove_key(hrecs, t1, tag_id)) > 0) {
-                    deleted++;
-                    break;
-                } else {
-                    return -1;
-                }
-            }
+        if ((ret = sam_hrecs_remove_key(hrecs, t1, tag_id)) == 1) {
+            deleted++;
+        } else if (ret == -1) {
+            return -1;
         }
+
         t1 = t1->next;
     } while (t1 != t2);
 
